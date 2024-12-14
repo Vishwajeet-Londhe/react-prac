de4ca988a9403e5815a061fbb7de49bfaebc8c6c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-14 22:00:29 +0530

    hello using useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 function App() {
+  const elem = useRef(null);
   return (
-    <div className='p-4'>
-      <form action="">
-        <input className='border-4' type="text" placeholder='name' />
-        <input type="submit" />
-      </form>
+    <div>
+      <div ref={elem} className='p-4'>
+        <h1>hello</h1>
+      </div>
     </div>
   )
 }
