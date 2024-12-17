16d4b5821168df5927d0248dea67535b0586b31b
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-17 21:21:21 +0530

    added email in it

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import React, { useRef } from 'react'
 function App() {
   const name = useRef(null);
   const age = useRef(null);
+  const email = useRef(null);
   
   const handleSubmit = (event)=>{
     event.preventDefault();
-    console.log(name.current.value, age.current.value)
+    console.log(name.current.value, age.current.value, email.current.value)
   }
 
   return (
     <form action="" onSubmit={handleSubmit}>
       <input ref={name} placeholder='name' type="text" />
       <input ref={age} placeholder='age' type="text" />
+      <input ref={email} placeholder='email' type="text" />
       <input type="submit" />
     </form>
   )
