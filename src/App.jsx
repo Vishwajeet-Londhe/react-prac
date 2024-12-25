fc7b73ca5424d616f9e919938abef00e06c42504
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-26 00:38:24 +0530

    hook installed and setup ready

--- src/App.jsx
+++ src/App.jsx
@@ -26,29 +26,41 @@
 
 //controlled components
 
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//   const [val, setVal] = useState({name:"",email:""});
+  
+//   const handleSubmit = (event)=>{
+//     event.preventDefault();
+//     console.log(val);
+//   }
+
+//   return (
+//     <div>
+//       <form action="" onSubmit={handleSubmit}>
+//         <input onChange={(event)=>setVal({...val, name:event.target.value})} type="text" placeholder='name' />
+//         <input onChange={(event)=>setVal({...val, email:event.target.value})} type="text" placeholder='email' />
+//         <input type="submit" />
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// react hook form
+
 import React from 'react'
-import { useState } from 'react'
 
 function App() {
-  const [val, setVal] = useState({name:"",email:""});
-  
-  const handleSubmit = (event)=>{
-    event.preventDefault();
-    console.log(val);
-  }
-
   return (
     <div>
-      <form action="" onSubmit={handleSubmit}>
-        <input onChange={(event)=>setVal({...val, name:event.target.value})} type="text" placeholder='name' />
-        <input onChange={(event)=>setVal({...val, email:event.target.value})} type="text" placeholder='email' />
-        <input type="submit" />
-      </form>
+      
     </div>
   )
 }
 
 export default App
-
-// react hook form
 
