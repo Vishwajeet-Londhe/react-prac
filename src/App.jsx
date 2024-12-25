1e8500b9fb3cc1ad32c88048b60912b8cea17923
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-26 00:29:37 +0530

    emai added to controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -26,25 +26,29 @@
 
 //controlled components
 
-// import React from 'react'
-// import { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-// function App() {
-//   const [val, setVal] = useState({name:""})
+function App() {
+  const [val, setVal] = useState({name:"",email:""});
   
-//   const handleSubmit = (event)=>{
-//     event.preventDefault();
-//     console.log(val);
-//   }
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(val);
+  }
 
-//   return (
-//     <div>
-//       <form action="" onSubmit={handleSubmit}>
-//         <input onChange={(event)=>setVal({name:event.target.value})} type="text" placeholder='name' />
-//         <input type="submit" />
-//       </form>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <input onChange={(event)=>setVal({...val, name:event.target.value})} type="text" placeholder='name' />
+        <input onChange={(event)=>setVal({...val, email:event.target.value})} type="text" placeholder='email' />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
 
-// export default App
+export default App
+
+// react hook form
+
